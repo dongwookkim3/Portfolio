5509d6da12c838e4e2ca04e67e88208dde103196
dongwookkim3 <dongwook.3.kim@gmail.com>
2024-12-11 00:58:52 +0900

    I want to sleep

--- src/data/about.ts
+++ src/data/about.ts
@@ -4,7 +4,8 @@ export const contestData = [
         title: '2024 하반기 전남대학교 PIMM 알고리즘 파티',
         date: '2024.09',
         problems: ['더블팰린드롬', '근성아 일하자'],
-        problemsNum: [32357, 32358]
+        problemsNum: [32357, 32358],
+        Nums: ['A', 'B']
     },
     {
         id: 'B',
@@ -22,15 +23,17 @@ export const contestData = [
         id: 'D',
         title: '가희와 함께 하는 7회 코딩테스트',
         date: '2024.11',
-        problems: ['가희와 4시간의 벽 1', '가희와 4시간의 벽 2', '가희와 서울 지하철 2호선', '가희와 부역명', '가희와 전기 요금 1', '가희와 클럽 오디션 3', '가희와 전기 요금 2', 'gahui and sousenkyo 7'],
-        problemsNum: [32775, 32776, 32777, 32778, 32779, 32780, 32783, 32786]
+        problems: ['가희와 4시간의 벽 1', '가희와 4시간의 벽 2', '가희와 서울 지하철 2호선', '가희와 부역명', '가희와 전기 요금 1', '가희와 클럽 오디션 3', 'gahui and sousenkyo 7', '가희와 전기 요금 2'],
+        problemsNum: [32775, 32776, 32777, 32778, 32779, 32780, 32786, 32783],
+        Nums: ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'I']
     },
     {
         id: 'E',
         title: '2024 서울시립대학교 프로그래밍 경진대회(UOSPC)',
         date: '2024.12',
         problems: ['UOS 문자열', '슈팅 연습', '드론 조작'],
-        problemsNum: [32929, 32930, 32932]
+        problemsNum: [32929, 32930, 32932],
+        Nums: ['A', 'B', 'D']
     },
     {
         id: 'F',
@@ -66,7 +69,7 @@ export const educationData = [
     {
         school: 'IT 꿈나무 성장지원 사업-화이트해커 양성교육',
         period: '2024.06 ~ 2024.11',
-        department: '정보보안교육'
+        department: '정보보안개론, 해킹(애플리케이션, 네트워크, 웹, 앱), 취약점 진단, 침해사고 대응, 악성코드 분석'
     },
     {
         school: 'National University of Singapore 디자인 교육',
@@ -100,13 +103,8 @@ export const certificateData = {
 
 export const algorithmContestData = [
     {
-        title: '2023 부산소프트웨어마이스터고등학교 교내 알고리즘 경진대회',
-        date: '2023.09',
-        link: 'https://credentials.nus.edu.sg/017d78c0-dd71-46db-9db6-2c3155a73787'
-    },
-    {
-        title: '2024 한국정보올림피아드 1차 대회',
-        date: '2024.05',
+        title: '2024 NYPC 1 Round',
+        date: '2024.08',
         link: 'https://credentials.nus.edu.sg/017d78c0-dd71-46db-9db6-2c3155a73787'
     },
     {
@@ -115,13 +113,18 @@ export const algorithmContestData = [
         link: 'https://credentials.nus.edu.sg/017d78c0-dd71-46db-9db6-2c3155a73787'
     },
     {
-        title: '2024 NYPC 1 Round',
-        date: '2024.08',
+        title: '2024 한국정보올림피아드 1차 대회',
+        date: '2024.05',
         link: 'https://credentials.nus.edu.sg/017d78c0-dd71-46db-9db6-2c3155a73787'
     },
     {
         title: 'Solved.ac Grand Arena Party',
         date: '2024.02',
+        link: 'https://credentials.nus.edu.sg/017d78c0-dd71-46db-9db6-2c3155a73787'
+    },
+    {
+        title: '2023 부산소프트웨어마이스터고등학교 교내 알고리즘 경진대회',
+        date: '2023.09',
         link: 'https://credentials.nus.edu.sg/017d78c0-dd71-46db-9db6-2c3155a73787'
     }
 ];
@@ -130,6 +133,7 @@ export const problemCreationData = [
     {
         title: '지원이의 여자친구',
         date: '2024.03',
-        platform: 'BOJ'
+        platform: 'CodeUP',
+        problemNum: 2054
     }
 ];
