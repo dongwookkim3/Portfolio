99057a8095068c23eb7709b476cfda89523ebd1f
dongwookkim3 <dongwook.3.kim@gmail.com>
2025-11-10 22:04:36 +0900

    .

--- src/data/about.ts
+++ src/data/about.ts
@@ -127,25 +127,11 @@ export const educationData: EducationData[] = [
         certificate_web: 'https://credentials.openrobotics.org/af4d1e8e-78d7-477c-b75a-3e8e195cfb84#acc.hCwPm9VL'
     },
     {
-        school: 'AI 커리어 스쿨 for 엔지니어',
-        period: '2024.12 ~ 2025.02',
-        department: '데이터 분석, 머신러닝, 딥러닝, LLM, 프롬프트 엔지니어링',
-        certificate_pdf_ko: '../../assets/certificates/education/AI커리어스쿨_수료증_김동욱.pdf',
-        certificate_pdf_en: '../../assets/certificates/education/AI커리어스쿨_수료증_KIM DONG WOOK.pdf'
-    },
-    {
         school: 'IT 꿈나무 성장지원사업 화이트해커 양성교육',
         period: '2024.06 ~ 2024.11',
         department: '정보보안개론, 해킹(애플리케이션, 네트워크, 웹, 앱), 취약점 진단, 침해사고 대응, 악성코드 분석',
         certificate_pdf_ko: '../../assets/certificates/education/IT_꿈나무_성징지원사업_화이트해커_양성교육.pdf',
         good:'../../assets/certificates/20250401210558238-1_rotated.pdf'
-    },
-    {
-        school: 'National University of Singapore 디자인 교육',
-        period: '2024.05',
-        department: 'Fundamentals of UI/UX Design',
-        certificate_pdf_en: '../../assets/certificates/education/National_University_of_Singapore_디자인교육.pdf',
-        certificate_web: 'https://credentials.nus.edu.sg/017d78c0-dd71-46db-9db6-2c3155a73787#acc.1vbBGTs3'
     }
 ];
 
